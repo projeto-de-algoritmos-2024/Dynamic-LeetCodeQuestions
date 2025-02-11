0d54b4f0a915ab7bb7ece503009248e82bbdf5e7
Alexandre Beck <alexandrebeck2003@gmail.com>
2025-02-10 22:14:14 -0300

    feat: first attempt (Wrong Answer)

---
+++ 5-longest-palindromic-substring.ts
@@ -0,0 +1,26 @@
+function longestPalindrome(s: string): string {
+  if (s.length <= 1) return s;
+
+  let start = 0,
+    maxLength = 1;
+
+  function expandAroundCenter(left: number, right: number): void {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left++;
+      right--;
+    }
+
+    const length = right - left - 1;
+    if (length > maxLength) {
+      start = left + 1;
+      maxLength = length;
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    expandAroundCenter(i, i);
+    expandAroundCenter(i, i + 1);
+  }
+
+  return s.substring(start, start + maxLength);
+}
